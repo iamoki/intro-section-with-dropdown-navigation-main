6e27abae5b01c0b66232093d491546c9eaea2a52
okhee <aoh1223@micehub.com>
2022-10-06 16:42:25 +0900

    ✅ nav, 햄버거메뉴 스크립트 추가

--- src/main.js
+++ src/main.js
@@ -1,12 +1,29 @@
 require('./scss/main.scss');
 
-let navEl = document.getElementById('nav');
+let isMenu = document.querySelectorAll('.isMenu');
+let moMenuBtn = document.querySelector('.mo-menu-btn');
+let headerWrap = document.querySelector('.header-wrap');
 
-navEl.addEventListener('click', function(e) {
+// 메뉴 on/off
+window.addEventListener('click', (e) => {
+    isMenu.forEach((e) => {
+        e.classList.remove('on');
+    })
+
     if(e.target.classList.contains('isMenu')) {
-        const parents = e.target.parentElement;
-        parents.classList.add('on');
-    } else {
-        return false;
+        e.target.classList.add('on');
+    }
+});
+
+// 모바일 햄버거메뉴 open/close
+moMenuBtn.addEventListener('click', () => {
+    headerWrap.classList.toggle('isOpen');
+});
+
+
+// resize 일정크기에서 햄버거메뉴 닫히기
+window.addEventListener('resize', (e) => {
+    if(!e.target.innerWidth <= 900) {
+        headerWrap.classList.remove('isOpen');
     }
 })
